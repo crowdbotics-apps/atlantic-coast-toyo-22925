d9842d11a5130e1c15942cc99dcf3543a99da7a1
Crowdbotics <team@crowdbotics.com>
2020-11-23 16:51:20 +0000

    Update components

---
+++ src/features/CopyOfBlankScreen5178102/screens/blank.js
@@ -0,0 +1,46 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+export class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+  state = { TextInput_5: "" }
+  render = () => (
+    <View>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        style={styles.TextInput_5}
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, marginHorizontal: 16 },
+  View_1: {},
+  TextInput_5: { fontFamily: "Roboto-Regular" }
+})
+
+function mapStateToProps(state) {
+  return { state: state.apiReducer }
+}
+const actionCreators = {}
+export default connect(mapStateToProps, actionCreators)(Blank)
